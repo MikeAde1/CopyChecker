183325ca68264d59f194a399a5217455b7a410a5
MikeAde1 <michaeladeneye1@gmail.com>
2019-09-13 17:04:50 +0100

    initial commit

---
+++ app/src/main/java/com/example/copychecker/viewModel/ClipBoardViewModelFactory.kt
@@ -0,0 +1,9 @@
+package com.example.copychecker.viewModel
+
+import android.app.Application
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.content.Context
+import kotlin.Suppress as Suppress1
+
+
